43b5f0043a04898ad92037c823b104e0d7ba3210
singuerinc <nahuel.scotti@gmail.com>
2020-02-02 02:33:08 +0100

    feat: clean

--- src/app.tsx
+++ src/app.tsx
@@ -1,8 +1,8 @@
 import { useMachine } from "@xstate/react";
 import produce from "immer";
 import * as React from "react";
-import { useReducer } from "react";
 import uuid from "uuid";
+import { assign } from "xstate";
 import { Add } from "./add";
 import { Failure } from "./failure";
 import { IBullet } from "./IBullet";
@@ -12,74 +12,44 @@ import { Loading } from "./loading";
 import { machine } from "./machine";
 import { Welcome } from "./welcome";
 
-type IBulletAction =
-  | { type: "UPDATE_ALL"; payload: IBullet[] }
-  | { type: "ADD_ONE" }
-  | { type: "UPDATE_ONE"; payload: IBullet };
-
 const equal = (x, y) => x === y;
 
-const reducer = (state: IBullet[], action: IBulletAction) => {
-  switch (action.type) {
-    case "ADD_ONE":
-      return produce(state, draft => {
-        draft.push({
-          id: uuid.v4(),
-          title: "Bullet",
-          date: new Date().toISOString(),
-          state: 0
-        });
-      });
-    case "UPDATE_ALL":
-      return action.payload;
-    case "UPDATE_ONE":
-      return state.map(item =>
-        equal(item.id, action.payload.id) ? action.payload : item
-      );
-    default:
-      throw new Error("no action");
-  }
-};
-
 export const App = () => {
-  const [state, dispatch] = useReducer<IBullet[]>(reducer, []);
   const [current, send] = useMachine(machine, {
     devTools: true,
     actions: {
       onWelcome: () => {
-        setTimeout(() => send({ type: "FETCH" }), 2000);
+        setTimeout(() => send({ type: "FETCH" }), 200);
       },
       load: () => {
-        const payload = () => {
-          try {
-            return JSON.parse(localStorage.getItem("journal")) || [];
-          } catch (e) {
-            return [];
-          }
-        };
-        dispatch({ type: "UPDATE_ALL", payload: payload() });
-        setTimeout(() => send({ type: "RESOLVE" }), 2000);
-        // fetch("./bullets.json")
-        //   .then(res => res.json())
-        //   .then((payload: IBullet[]) => {
-        // })
-        // .catch(() => send({ type: "ERROR" }));
-      }
+        const payload = JSON.parse(localStorage.getItem("journal")) || [];
+        send({ type: "RESOLVE", payload });
+      },
+      updateOne: assign((context, event) => ({
+        journal: context.journal.map(item =>
+          equal(item.id, event.payload.id) ? event.payload : item
+        )
+      })),
+      addOne: assign(context =>
+        produce(context, draft => {
+          draft.journal.push({
+            id: uuid.v4(),
+            title: "Bullet",
+            date: new Date().toISOString(),
+            state: 0
+          });
+        })
+      )
     }
   });
 
   function onUpdate(payload: IBullet) {
-    dispatch({ type: "UPDATE_ONE", payload });
-    send({ type: "SAVE", payload: state }); //!FIXME: this state is old
+    send({ type: "UPDATE_ONE", payload });
   }
 
   function onAdd() {
-    console.log("SAVE one!!!!!!!!!");
-    dispatch({ type: "ADD_ONE" });
-    send({ type: "SAVE", payload: state }); //!FIXME: this state is old
+    send({ type: "ADD_ONE" });
   }
-
-  console.log(current.value);
 
   return (
     (current.matches("welcome") && <Welcome />) ||
@@ -88,7 +58,7 @@ export const App = () => {
     (current.matches("journal") && (
       <>
         <Add onAdd={onAdd} />
-        <Journal data={state} onUpdate={onUpdate} />
+        <Journal data={current.context.journal} onUpdate={onUpdate} />
         {current.matches("journal.save") && (
           <i>
             <img src={sync} />
--- src/machine.ts
+++ src/machine.ts
@@ -1,7 +1,22 @@
-import { Machine } from "xstate";
+import { Machine, assign } from "xstate";
+import { IBullet } from "./IBullet";
 
-export const machine = Machine({
+interface JournalContext {
+  journal: IBullet[];
+}
+
+type JournalEvent =
+  | { type: "FETCH" }
+  | { type: "RESOLVE"; payload: IBullet[] }
+  | { type: "ADD_ONE" }
+  | { type: "UPDATE_ONE"; payload: IBullet }
+  | { type: "ERROR" };
+
+export const machine = Machine<JournalContext, any, JournalEvent>({
   strict: true,
+  context: {
+    journal: []
+  },
   initial: "welcome",
   states: {
     welcome: {
@@ -9,11 +24,16 @@ export const machine = Machine({
       entry: ["onWelcome"]
     },
     loading: {
+      entry: ["load"],
       on: {
-        RESOLVE: "journal",
+        RESOLVE: {
+          target: "journal",
+          actions: assign({
+            journal: (_, event) => event.payload
+          })
+        },
         ERROR: "failure"
-      },
-      entry: ["load"]
+      }
     },
     failure: {},
     journal: {
@@ -21,21 +41,28 @@ export const machine = Machine({
       states: {
         default: {
           on: {
-            SAVE: "save"
+            ADD_ONE: "add",
+            UPDATE_ONE: "update"
           }
         },
-        failure: {},
+        add: {
+          entry: ["addOne"],
+          on: {
+            "": "save"
+          }
+        },
+        update: {
+          entry: ["updateOne"],
+          on: {
+            "": "save"
+          }
+        },
         save: {
           invoke: {
-            src: (_, event) => {
-              console.log("SAVE!!!!!!!!!", event.payload);
-
-              localStorage.setItem("journal", JSON.stringify(event.payload));
-
+            src: ctx => {
+              localStorage.setItem("journal", JSON.stringify(ctx.journal));
               return new Promise(resolve => {
-                setTimeout(() => {
-                  resolve();
-                }, 2000);
+                setTimeout(resolve, 200);
               });
             },
             onDone: {
