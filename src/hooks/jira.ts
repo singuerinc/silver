1b3f8ce4ee4c6ab96b808e741950650190195a3e
singuerinc <nahuel.scotti@gmail.com>
2020-02-04 22:02:21 +0100

    refactor: links

--- src/hooks/jira.ts
+++ src/hooks/jira.ts
@@ -1,8 +1,8 @@
 const jiraReplacer = (_, p1: string) =>
   [
-    `<a data-hook='jira' href='https://jira.netent.com/browse/${p1}' target='blank'>`,
+    `<a data-hook='jira' href='https://jira.com/browse/${p1}' target='blank'>`,
     p1,
     "</a>"
   ].join("");
 
-export const jira = text => text.replace(/(KEYPAD-[\d]*)/gi, jiraReplacer);
+export const jira = text => text.replace(/(JIRA-[\d]*)/gi, jiraReplacer);
